35845d0ea45b447ef543f2f026c2e4b7c0beddc2
ArtRosh <artem.rosh17@gmail.com>
2025-06-24 12:21:30 -0700

    Add initial Layout component

---
+++ src/components/Layout.jsx
@@ -0,0 +1,12 @@
+function Layout() {
+  return (
+    <div>
+      <header>
+        <h1>My App</h1>
+      </header>
+      {/* Main content will go here */}
+    </div>
+  );
+}
+
+export default Layout;
