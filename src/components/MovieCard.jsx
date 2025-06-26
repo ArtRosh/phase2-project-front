3e7534f0abb1ae73d6fe62c1541a4fea2bf6a11b
ArtRosh <artem.rosh17@gmail.com>
2025-06-26 13:16:11 -0700

    Make movie title a link to movie detail page

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom";
 
 
 function MovieCard({ movie }) {
     return (
         <li>
             <img src={movie.poster} alt={movie.title} />
-            <h3>{movie.title}</h3>
+            <h3>
+                <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+            </h3>
             <p>Year: {movie.year}</p>
             <p>Director: {movie.director}</p>
             <p>Rating: {movie.rating}</p>
