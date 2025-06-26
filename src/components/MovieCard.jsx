6a7993f6532923d3c14fcbca2a59dfdfcf71f02d
ArtRosh <artem.rosh17@gmail.com>
2025-06-26 12:10:09 -0700

    Show movie poster, year, director, and rating in MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,7 +3,11 @@
 function MovieCard({ movie }) {
     return (
         <li>
+            <img src={movie.poster} alt={movie.title} />
             <h3>{movie.title}</h3>
+            <p>Year: {movie.year}</p>
+            <p>Director: {movie.director}</p>
+            <p>Rating: {movie.rating}</p>
         </li>
     )
 }
