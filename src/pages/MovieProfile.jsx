aa6bad3a23e83997e5e8c5998979e7631e80723f
ArtRosh <artem.rosh17@gmail.com>
2025-06-26 14:25:11 -0700

    Add fetching logic to MovieProfile page

--- src/pages/MovieProfile.jsx
+++ src/pages/MovieProfile.jsx
@@ -1,8 +1,16 @@
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function MovieProfile() {
     
     const {id} = useParams();
+    const [movie, setMovie] = useState(null);
+
+    useEffect(() => {
+        fetch(`http://localhost:6001/movies/${id}`)
+        .then(res => res.json())
+        .then(data => setMovie(data))
+    }, [id]);
 
     return <h2>Movie ID: {id}</h2>
 }
