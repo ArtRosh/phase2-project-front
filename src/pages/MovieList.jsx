272a771c8bdbd973e35cf80a1be6eb1c74cb3e2d
ArtRosh <artem.rosh17@gmail.com>
2025-06-26 11:49:24 -0700

    Render MovieCard for each movie in MovieList

--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -1,7 +1,26 @@
-
+import { useState, useEffect } from "react"
+import MovieCard from "../components/MovieCard";
 
 function MovieList() {
-    return <h2>MovieList</h2>
+    const[movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:6001/movies")
+        .then(res => res.json())
+        .then(data => (
+            setMovies(data)
+        ))
+    }, [])
+    return (
+        <div>
+            <h2>MovieList</h2>
+            <ul>
+                {movies.map(movie => (
+                    <MovieCard key={movie.id} movie={movie} />
+                ))}
+            </ul>
+        </div>
+    )
 }
 
 export default MovieList;
