23f93d8653344d97cbe8ebda5a695fa7b09b2b6d
ArtRosh <artem.rosh17@gmail.com>
2025-06-26 20:49:56 -0700

    Add and connect Layout.css for overall app styling

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,11 @@
 import NavBar from "./NavBar";
 import { Outlet } from "react-router-dom";
+import "./Layout.css"
 
 function Layout() {
   return (
-    <div>
-      <header>
+    <div className="layout-container">
+      <header className="layout-header">
         <h1>My App</h1>
         <NavBar />
       </header>
