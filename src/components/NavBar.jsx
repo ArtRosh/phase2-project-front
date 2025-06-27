2540ccdbc814d567537685eb2d48a025b746bbbc
ArtRosh <artem.rosh17@gmail.com>
2025-06-26 17:58:49 -0700

    Add and connect separate CSS file for NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom"
+import "./NavBar.css"
 
 function NavBar() {
     return (
-        <nav>
-            <ul>
+        <nav className="navbar">
+            <ul className="navbar-list">
                 <li>
                     <Link to="/">Home</Link>
                 </li>
