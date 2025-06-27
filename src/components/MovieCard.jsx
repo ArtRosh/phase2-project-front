7719447337e4b65414ed0c3c5a69c09abedce28e
ArtRosh <artem.rosh17@gmail.com>
2025-06-26 17:14:38 -0700

    Add separate CSS file and connect styles for MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
+import "./MovieCard.css"
 
 
 function MovieCard({ movie }) {
     return (
         <li className="movie-card">
             <img src={movie.poster} alt={movie.title} />
-            <h3>
+            <h3 className="movie-card-title">
                 <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
             </h3>
             <p>Year: {movie.year}</p>
